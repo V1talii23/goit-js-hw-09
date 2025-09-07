d8c34b4531a6ba94f6edf0f3939a8450ef95bda2
Vitalii Savchuk <vetalsavchuk23@gmail.com>
2025-09-07 12:57:07 +0100

    hw-09

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,30 @@
-console.log('Gallery');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { images } from './images';
+
+const ul = document.querySelector('.gallery');
+
+const markup = images
+  .map(
+    ({ preview, original, description }) =>
+      `
+  <li class="gallery-item">
+ <a class="gallery-link" href="${original}" >
+ <img
+  class="gallery-image" 
+  src="${preview}" 
+  alt="${description}"
+  />
+ </a>
+</li>
+  `
+  )
+  .join('');
+
+ul.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,58 @@
-console.log('Form');
+const DATA_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const input = form.querySelector('input');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('submit', handlerSubmit);
+form.addEventListener('input', handlerInput);
+
+populateData();
+
+function handlerInput(event) {
+  const { name, value } = event.target;
+
+  if (formData.hasOwnProperty(name)) {
+    formData[name] = value.trim();
+
+    localStorage.setItem(DATA_KEY, JSON.stringify(formData));
+  }
+}
+
+function populateData() {
+  try {
+    const savedData = JSON.parse(localStorage.getItem(DATA_KEY));
+    if (!savedData) return;
+
+    formData = { ...formData, ...savedData };
+
+    input.value = formData.email || '';
+    textarea.value = formData.message || '';
+  } catch (error) {
+    console.error('I catch an error:', error);
+    localStorage.removeItem(DATA_KEY);
+  }
+}
+
+function handlerSubmit(e) {
+  e.preventDefault();
+
+  const email = input.value.trim();
+  const message = textarea.value.trim();
+
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return;
+  } else {
+    console.log(formData);
+    formData = { email: '', message: '' };
+
+    e.target.reset();
+    localStorage.removeItem(DATA_KEY);
+  }
+}
